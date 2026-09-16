a9c4878fa4b359785b977d66a6a0189ca1575ebe
jieun78042500-ui <jieun78042500@gmail.com>
2026-09-16 16:11:17 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
 import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+const config: NextConfig = { output: 'export', trailingSlash: true, images: { unoptimized: true } };
+export default config;
